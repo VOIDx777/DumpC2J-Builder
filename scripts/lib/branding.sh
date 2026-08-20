c6f4c1e3b56c00c4aaf593fd0d5d83b4ed35c525
VOID_0x777 <109896636+VOIDx777@users.noreply.github.com>
2026-08-20 14:14:13 +0530

    Delete scripts directory

--- scripts/lib/branding.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ -n "$MODULES_DIR" ] && [ -n "$REPO_NAME" ] && [ -f "$MODULES_DIR/$REPO_NAME/kernel/Makefile" ]; then
-  echo "[+] Applying custom kernel branding..."
-  python3 "${BUILDER_DIR}/scripts/branding.py" "$MODULES_DIR/$REPO_NAME/kernel/Makefile" "DumpC2J"
-fi
